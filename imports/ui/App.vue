5b273cd24df293c7deb506ee444cf5ef0bd76c48
hasibul-99 <hasibul_uap@outlook.com>
2018-02-02 23:26:03 +0600

    Meteor vue.js bootstrap scss project

---
+++ imports/ui/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="tushar">
+    <h2>Simple Meteor example using Vue 2.x</h2>
+
+    <div class="row">
+      <div class=" red">
+
+      </div>
+      <div class=" green">
+
+      </div>
+    </div>
+    <span class="border border-primary"></span>
+    <span class="border border-secondary"></span>
+    <span class="border border-success"></span>
+    <span class="border border-danger"></span>
+    <span class="border border-warning"></span>
+    <span class="border border-info"></span>
+    <span class="border border-light"></span>
+    <span class="border border-dark"></span>
+    <span class="border border-white"></span>
+  </div>
+
+
+</template>
+
+<script>
+export default {
+}
+</script>
